4ddc841568a01529510afba40edd1cf8470e3c9a
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-04-10 14:40:34 +0100

    refactor

--- validatePassword.js
+++ validatePassword.js
@@ -1,12 +1,9 @@
 function validatePassword(password) {
-	if (
-		password.length >= 8 &&
-		/[a-z]/g.test(password) &&
-		/[1-9]/g.test(password)
-	) {
-		return true;
-	}
-	return false;
+	const lengthCheck = password.length >= 8;
+	const letterCheck = /[a-z]/g.test(password);
+	const numberCheck = /[1-9]/g.test(password);
+
+	return lengthCheck && letterCheck && numberCheck;
 }
 
 module.exports = validatePassword;
